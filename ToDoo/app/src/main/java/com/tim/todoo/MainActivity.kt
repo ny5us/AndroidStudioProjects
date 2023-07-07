f2324632abb9ecbaebd68e9c26b87ffd7bae3456
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-07 08:57:19 -0700

    Not Working Again.
    App crashed when I started adding functionality for adding to the list. List was not displaying properly even before that.

--- ToDoo/app/src/main/java/com/tim/todoo/MainActivity.kt
+++ ToDoo/app/src/main/java/com/tim/todoo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tim.todoo
 
+import android.R.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var taskList = arrayListOf<String>()
+
+        var arrayAdapter = ArrayAdapter<String>(this,
+            layout.simple_list_item_multiple_choice,taskList)
+        addButton.setOnClickListener {
+            taskList.add(newTaskEntry.text.toString())
+
+            toDoListView.adapter = arrayAdapter
+
+            arrayAdapter.notifyDataSetChanged()
+
+            newTaskEntry.text.clear()
+        }
     }
 }

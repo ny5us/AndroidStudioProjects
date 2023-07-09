02365579403a2c0f22fe89bcc6ab97bb2f6703ce
ny5us <133588054+ny5us@users.noreply.github.com>
2023-07-09 12:59:25 -0700

    Update MainActivity.kt

--- ToDoo/app/src/main/java/com/tim/todoo/MainActivity.kt
+++ ToDoo/app/src/main/java/com/tim/todoo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tim.todoo
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.SparseBooleanArray
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.tim.todoo.databinding.ActivityMainBinding
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         var taskList = arrayListOf<String>()
         val items = listOf<String>("hi", "hello")
-        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, taskList)
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, taskList)
 
         binding.plusButton.setOnClickListener {
             taskList.add(binding.newTaskEntry.text.toString())
@@ -29,6 +30,18 @@ class MainActivity : AppCompatActivity() {
 
             arrayAdapter.notifyDataSetChanged()
             binding.newTaskEntry.text.clear()
+        }
+        binding.minusButton.setOnClickListener {
+
+            val positionChecked: SparseBooleanArray = binding.toDoListView.checkedItemPositions
+            var deleteItem = binding.toDoListView.count -1
+            while(deleteItem >= 0) {
+                if (positionChecked.get(deleteItem)) {
+                    arrayAdapter.remove(taskList.get(deleteItem))
+                }
+                deleteItem--
+            }
+
         }
     }
 }
